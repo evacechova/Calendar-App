8775ae3e807135cdd8a2dea9ab4eaad96480f6a5
Eva Čechová <cechovaev@gmail.com>
2024-02-29 18:44:39 +0100

    made month change functional

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,32 @@
-console.log('Ahoj');
+const monthNav = ["leden", "únor", "březen", "duben", "květen", "červen", "červenec", "srpen", "září", "říjen", "listopad", "prosinec"];
+let index = 0;
+
+const setMonth = (monthName) => {
+    const month = document.querySelector('#month');
+    month.textContent = monthName;
+}
+
+const nextMonth = () => {
+    index++
+    
+    if (index === monthNav.length) {
+        index = 0;
+    }
+    
+    setMonth(monthNav[index]);
+}
+
+const previousMonth = () => {
+    index--
+
+    if (index === -1) {
+        index = monthNav.length -1;
+    }
+
+    setMonth(monthNav[index]);
+}
+
+setMonth(monthNav[index]);
+document.querySelector('#next').addEventListener('click', nextMonth);
+document.querySelector('#previous').addEventListener('click', previousMonth);
+
