f3dcafa3b53ff97d26e2a5791abc3790a0b9e2a7
Eva Čechová <cechovaev@gmail.com>
2024-02-29 20:24:33 +0100

    restructured calendar body

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,10 @@
 let today = new Date()
 let thisMonth = today.getMonth();
+let thisYear = today.getFullYear();
 
 const monthNav = ["leden", "únor", "březen", "duben", "květen", "červen", "červenec", "srpen", "září", "říjen", "listopad", "prosinec"];
 let index = 0;
-let year = 2024;
+let year = thisYear;
 
 const setMonth = (monthName) => {
     const month = document.querySelector('#month');
@@ -15,9 +16,10 @@ const nextMonth = () => {
     
     if (index === monthNav.length) {
         index = 0;
+        year++
     }
     
-    setMonth(monthNav[index]);
+    setMonth(`${monthNav[index]} ${year}`);
 }
 
 const previousMonth = () => {
@@ -25,12 +27,13 @@ const previousMonth = () => {
 
     if (index === -1) {
         index = monthNav.length -1;
+        year--
     }
 
-    setMonth(monthNav[index]);
+    setMonth(`${monthNav[index]} ${year}`);
 }
 
-setMonth(monthNav[thisMonth]);
+setMonth(`${monthNav[thisMonth]} ${thisYear}`);
 document.querySelector('#next').addEventListener('click', nextMonth);
 document.querySelector('#previous').addEventListener('click', previousMonth);
 
