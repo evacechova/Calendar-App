f0cf160c9a4827f42d8cdefe2b4607b96a80747d
Eva Čechová <cechovaev@gmail.com>
2024-03-03 22:23:55 +0100

    adjusted create button styling

--- src/js/main.js
+++ src/js/main.js
@@ -30,7 +30,7 @@ const nextMonth = () => {
 
     dayPointer = firstDayOfMonth (year, monthIndex);
     monthLength = daysInMonth(year, monthIndex);
-    dayToDivAsignment();
+    dayToDivAssignment();
 }
 
 const previousMonth = () => {
@@ -50,7 +50,7 @@ const previousMonth = () => {
 
     dayPointer = firstDayOfMonth (year, monthIndex);
     monthLength = daysInMonth(year, monthIndex);
-    dayToDivAsignment();
+    dayToDivAssignment();
 }
 
 setMonth(`${monthNav[thisMonth]} ${thisYear}`);
@@ -75,7 +75,7 @@ let dayPointer = firstDayOfMonth(year, thisMonth);
 
 let monthLength = daysInMonth(year, thisMonth);
 
-const dayToDivAsignment = () => {
+const dayToDivAssignment = () => {
     console.log(dayPointer);
     if (dayPointer === 0) {
         dayPointer = 6;
@@ -91,14 +91,27 @@ const dayToDivAsignment = () => {
     }
 }
 
-dayToDivAsignment();
+dayToDivAssignment();
+
+const divHighlightOn = () => {
+    for(const div of dayDivs) {
+        div.addEventListener('click', () => {    
+            for(const div of dayDivs) {
+                div.classList.remove('markedDiv');
+            }
+            div.classList.add('markedDiv');
+        })
+    }
+}
+
+divHighlightOn();
 
 //----------------------------------------------------
 
 //Modal
 
 const modal = document.querySelector('#modal');
-const openModalArray = document.querySelectorAll('.day-no');
+const openModalArray = document.querySelectorAll('.event__create-button');
 const closeModal = document.querySelector('.modal__close');
 
 for (const modalButton of openModalArray) {
@@ -110,3 +123,61 @@ for (const modalButton of openModalArray) {
 closeModal.addEventListener('click', () => {
     modal.close();
 })
+
+//Mobal submit button
+
+let modalSubmitButton = document.querySelector("#modal__submit-button");
+
+modalSubmitButton.addEventListener('click', (e) => {
+    if(document.querySelector('#form').checkValidity()) {
+
+    e.preventDefault();
+
+    let inputEventDescription = document.querySelector('#event-description');
+    //console.log(inputEventDescription.value);
+    const eventDecritptionSaved = document.querySelector('#event-description--saved');
+    eventDecritptionSaved.textContent = inputEventDescription.value;
+
+    let inputStartDate = document.querySelector('#start-date');
+    //console.log(inputStartDate.value);
+    const startDateFormat = new Date (inputStartDate.value);
+    let inputEndDate = document.querySelector('#end-date');
+    //console.log(inputEndDate.value);
+    const endDateFormat = new Date (inputEndDate.value);
+
+    const options = {
+        weekday: "short",
+        day: "numeric",
+        month: "numeric",
+        year: "numeric",
+    }
+
+    const eventDateSaved = document.querySelector('#event-date--saved')
+    eventDateSaved.textContent = `${startDateFormat.toLocaleString('cs-CZ', options)} – ${endDateFormat.toLocaleString('cs-CZ', options)}`;
+
+    let form = document.querySelector('#form');
+
+    form.submit();
+    }
+});
+
+//Test event
+
+const testEvent = {
+    eventDescription: "Filmový festival",
+    eventStart: "2024-03-12",
+    eventEnd: "2024-03-14"
+}
+
+const highlightDayWithEvent = (argEvent, argYear, argMonth, argDay) => {
+    const [year, month, day] = argEvent.eventStart.split('-');
+    
+    if(year !== argYear) {
+        return;
+    }
+    if(month !== argMonth) {
+        return;
+    }
+    for()
+    
+}
